8d12912085b113686c889750d4d1668767164f54
ivan <moscoso@gmail.com>
2014-12-07 18:35:14 -0600

    Initial Commit

---
+++ ColorMeAnsi/ColorMeAnsi.h
@@ -0,0 +1,19 @@
+//
+//  ColorMeAnsi.h
+//  ColorMeAnsi
+//
+//  Created by Ivan M on 12/7/14.
+//  Copyright (c) 2014 3boxed. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ColorMeAnsi.
+FOUNDATION_EXPORT double ColorMeAnsiVersionNumber;
+
+//! Project version string for ColorMeAnsi.
+FOUNDATION_EXPORT const unsigned char ColorMeAnsiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorMeAnsi/PublicHeader.h>
+
+
